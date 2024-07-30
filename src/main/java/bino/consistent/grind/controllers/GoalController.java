81c2a57dd67a47d6661440270e0d68b0537e2dfa
Sam Bakri <jerry.monclaire@outlook.com>
2024-07-29 22:57:49 -0500

    Getting back to it. Updating the goal controller and service to accomodate more endpoints. In progress!

--- src/main/java/bino/consistent/grind/controllers/GoalController.java
+++ src/main/java/bino/consistent/grind/controllers/GoalController.java
@@ -1,31 +1,79 @@
 package bino.consistent.grind.controllers;
 import bino.consistent.grind.entities.*;
 import bino.consistent.grind.services.*;
+import jakarta.validation.Valid;
+
 import java.util.List;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/goals")
 public class GoalController {
+    // @Autowired
+    // private GoalService goalService;
+
+    // @PostMapping
+    // public Goal createGoal(@RequestBody Goal goal) {
+    //     return goalService.createGoal(goal);
+    // }
+
+    // @GetMapping("/user/{userId}")
+    // public List<Goal> getGoalsByUser(@PathVariable Long userId) {
+    //     return goalService.getGoalsByUserId(userId);
+    // }
+
+    // @DeleteMapping
+    // public void deleteGoal(@RequestBody Goal goal){
+    //     goalService.deleteGoal(goal);
+    // }
+
     @Autowired
     private GoalService goalService;
 
-    @PostMapping
-    public Goal createGoal(@RequestBody Goal goal) {
-        return goalService.createGoal(goal);
+    GoalController(GoalService goalService) {
+        this.goalService = goalService;
     }
 
-    @GetMapping("/user/{userId}")
-    public List<Goal> getGoalsByUser(@PathVariable Long userId) {
-        return goalService.getGoalsByUserId(userId);
+    @GetMapping
+    public List<Goal> findAll() {
+        return goalService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Goal findById(@PathVariable Long id) {
+        return goalService.findById(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public Goal create(@RequestBody Goal goal) {
+        return goalService.create(goal);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void update(@Valid @RequestBody Goal goal, @PathVariable Long id) {
+        goalService.update(goal,id);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        goalService.delete(id);
     }
 
     // Other goal-related endpoints
--- src/main/java/bino/consistent/grind/services/GoalService.java
+++ src/main/java/bino/consistent/grind/services/GoalService.java
@@ -1,22 +1,68 @@
 package bino.consistent.grind.services;
 import bino.consistent.grind.repositories.*;
+import jakarta.validation.Valid;
 import bino.consistent.grind.entities.*;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Service
 public class GoalService {
     @Autowired
     private GoalRepository goalRepository;
 
-    public Goal createGoal(Goal goal) {
+    // GoalService(GoalRepository goalRepository){
+    //     this.goalRepository = goalRepository;
+    // }
+
+    // public Goal createGoal(Goal goal) {
+    //     return goalRepository.save(goal);
+    // }
+
+    // public List<Goal> getGoalsByUserId(Long userId) {
+    //     return goalRepository.findByUserId(userId);
+    // }
+
+    // public void deleteGoal(Integer id) {
+    //     goalRepository.delete();
+    // }
+
+    public List<Goal> findAll() {
+        return goalRepository.findAll();
+    }
+
+    public Goal findById(@PathVariable Long id) {
+        return goalRepository.findById(id).get();
+    }
+
+    
+    public Goal create(@RequestBody Goal goal) {
         return goalRepository.save(goal);
     }
 
-    public List<Goal> getGoalsByUserId(Long userId) {
-        return goalRepository.findByUserId(userId);
+    
+    public void update(@Valid @RequestBody Goal goal, @PathVariable Long id) {
+        if (goalRepository.existsById(id)){
+            this.delete(id);
+            this.create(goal);
+        }
+        else {
+            this.create(goal);
+        }
+    }
+
+    
+    public void delete(@PathVariable Long id) {
+        goalRepository.deleteById(id);
     }
 
     // Other goal-related methods
